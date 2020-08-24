9b2a3f11b90aa2bb8d4a0c5ad61b8b569b1a732d
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-08-24 08:03:50 -0500

    Update RightToLeftSolution.py

--- Python/Roman-to-Integer/RightToLeftSolution.py
+++ Python/Roman-to-Integer/RightToLeftSolution.py
@@ -1,1 +1,19 @@
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
 
+class RightToLeftSolution:
+    def romanToInt(self, s: str) -> int:
+        total = values.get(s[-1])
+        for i in (range(len(s) - 1)):
+            if values[s[i]] < values[s[i + 1]]:
+                total -= values[s[i]]
+            else:
+                total += values[s[i]]
+        return total
