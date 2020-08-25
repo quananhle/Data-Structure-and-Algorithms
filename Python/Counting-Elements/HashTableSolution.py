7a87d74e5a30cec4f7e4bcf8a97454100e0bbb8f
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-08-25 12:20:07 -0500

    Update HashTableSolution.py

--- Python/Counting-Elements/HashTableSolution.py
+++ Python/Counting-Elements/HashTableSolution.py
@@ -7,7 +7,9 @@ class Solution(object):
         #set is faster than list in finding element inside it
         hash_set = set(arr)
         count = 0
+        #for each element x in arr
         for x in arr:
+            #finding x+1 in the hash table yields result as elements are unique, duplicates are removed
             if x + 1 in hash_set:
                 count += 1
         return count
