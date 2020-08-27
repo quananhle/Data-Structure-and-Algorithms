8cad5602d2ef3422491fab89339c1d6720a286ba
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-08-26 23:54:19 -0500

    Create ShortSolution.py

---
+++ Python/Move-Zeroes/ShortSolution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        #loop backward 1 step at a time
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i] == 0:
+                del nums[i]
+                nums.append(0)
