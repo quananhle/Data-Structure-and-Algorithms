fedb68f041d77ca4d6d2eafa67e3e058e421e636
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-08-26 22:42:33 -0500

    Create OptimizingSolution.py

---
+++ Python/Last-Stone-Weight/OptimizingSolution.py
@@ -0,0 +1,7 @@
+class OptimizingSolution:
+    def lastStoneWeight(self, stones):
+        while len(stones) != 1:
+            stones = sorted(stones)
+            last = stones.pop()
+            stones[len(stones)-1] = last - stones[len(stones)-1]
+        return stones[0]
