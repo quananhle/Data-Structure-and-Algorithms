be3f850dc0c52cb114b9f741c6d135eabc099fc1
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-08-30 21:31:41 -0500

    Create Set.py

---
+++ Python/Single-Number/Set.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #initialize a set
+        num_set = set()
+        #for each element in list nums
+        for num in nums:
+            #if duplicate/second element of the same num found
+            if num in num_set:
+                #remove from num_set
+                num_set.remove(num)
+            else:
+                #otherwise, add element to num_set
+                num_set.add(num)
+        #return the only element left or the first element in num_set
+        return num_set.pop()
