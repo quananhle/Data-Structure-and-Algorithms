b8301da0b63b21198b93c258d4bcd5aef3b77fcb
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-08-30 19:48:26 -0500

    Create HashTable.py

---
+++ Python/Single-Number/HashTable.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #initialize a hash table object
+        hash_table = defaultdict(int)
+        #for each num in list nums
+        for num in nums:
+            #add value to key if duplicate is met
+            hash_table[num] += 1
+        #for each key in hash table
+        for key in hash_table:
+            #if key has 1 value
+            if hash_table[key] == 1:
+                #return key
+                return key
+            
