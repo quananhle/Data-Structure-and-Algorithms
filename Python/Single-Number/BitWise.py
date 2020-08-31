a3107d7fd955fec801319465f3d1c403b5cee726
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-08-30 21:16:52 -0500

    Create BitWise.py

---
+++ Python/Single-Number/BitWise.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
