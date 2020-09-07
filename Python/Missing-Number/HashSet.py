e5e0b5abb319f6e78a6890767a9031cf05fcd696
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-09-06 21:56:58 -0500

    Create HashSet.py

---
+++ Python/Missing-Number/HashSet.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #initialize a set of distinct elements in list nums
+        num_set = set(nums)
+        #for number in range of [0..n+1] to have space for only 1 missing number
+        for number in range(len(nums) + 1):
+            #if a number in range is not num_set
+            if number not in num_set:
+                #return the missing number
+                return number
+                
