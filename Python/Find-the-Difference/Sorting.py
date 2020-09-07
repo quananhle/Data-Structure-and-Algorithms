bf3cad08b2053386d78b677b337ef41464e391b9
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-09-07 00:30:32 -0500

    Update Sorting.py

--- Python/Find-the-Difference/Sorting.py
+++ Python/Find-the-Difference/Sorting.py
@@ -6,8 +6,13 @@ class Sorting(object):
         :rtype: str
         """
         i = 0
+        #loop as far as length of string s
         while i < len(s):
+            #if the letter at index i of sorted string s is difference than the letter at index i of sorted string t
             if sorted(s)[i] != sorted(t)[i]:
+                #return the letter in string t
                 return sorted(t)[i]
+            #increment one letter at at time
             i += 1
+        #otherwise, the letter at the last index of sorted string t is the different letter
         return sorted(t)[i]
