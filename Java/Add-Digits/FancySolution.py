08b94b8130e8e5afaf9def82a4e93804090bceeb
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-09-26 04:41:00 -0500

    Update FancySolution.py

--- Java/Add-Digits/FancySolution.py
+++ Java/Add-Digits/FancySolution.py
@@ -1,6 +1,7 @@
 class FancySolution {
     public int addDigits(int num) {
       if (num == 0) {return 0;}
-      elif (num % 9 == 0) {return 9;}
+      else if (num % 9 == 0) {return 9;}
       else {return num % 9;}
+    }
 }
