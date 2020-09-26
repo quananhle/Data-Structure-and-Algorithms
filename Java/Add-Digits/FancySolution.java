123d039eb364bdb2d65b2d794302abc6c5aad519
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-09-26 04:44:09 -0500

    Update FancySolution.java

--- Java/Add-Digits/FancySolution.java
+++ Java/Add-Digits/FancySolution.java
@@ -3,6 +3,7 @@ class FancySolution {
 //         if (num == 0) {return 0;}
 //         else if (num % 9 == 0) {return 9;}
 //         else {return num % 9;}
+        // return 0 if num = 0 else return num % 9
         return num == 0 ? 0 : 1 + (num - 1) % 9;
     }
 }
