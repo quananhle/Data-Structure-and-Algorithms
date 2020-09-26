ce916c294c841cef8916deca818738151d6adbc2
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-09-26 03:57:10 -0500

    Update FancySolution.py

--- Python/Add-Digits/FancySolution.py
+++ Python/Add-Digits/FancySolution.py
@@ -4,9 +4,9 @@ class FancySolution(object):
         :type num: int
         :rtype: int
         """
-        if num == 0:
-            return 0
-        if num % 9 == 0:
-            return 9
-        return num % 9
+        # if num == 0:
+        #     return 0
+        # if num % 9 == 0:
+        #     return 9
+        # return num % 9
         return 1 + (num - 1) % 9 if num else 0
