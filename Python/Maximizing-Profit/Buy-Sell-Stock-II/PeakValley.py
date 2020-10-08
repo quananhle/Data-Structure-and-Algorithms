75960fc440ef232c53d5a8c8fbf0ac24a6cf499f
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-10-07 23:53:35 -0500

    Update PeakValley.py

--- Python/Maximizing-Profit/Buy-Sell-Stock-II/PeakValley.py
+++ Python/Maximizing-Profit/Buy-Sell-Stock-II/PeakValley.py
@@ -9,17 +9,18 @@ class PeakValley(object):
         max_profit   = 0
         #loop until the last element of list prices
         while current_idx < len(prices)-1:
-            #if not at the last index and value of the current index is higher than the value of the next index
+            #proof: if not at the last index and value of the current index is higher than the value of the next index
             if current_idx < len(prices)-1 and prices[current_idx] >= prices[next_idx]:
                 #move from current index to the next
                 current_idx += 1
-            #otherwise, store the value of current index as the floor/valley value
+            #store the value of current index as the floor/valley value
             valley_price = prices[current_idx]
-            #loop if not at the last index and value of the current index is lower than the value of the next index            
+            #proof: loop if not at the last index and value of the current index is lower than the value of the next index            
             if current_idx < len(prices)-1 and prices[current_idx] <= prices[next_idx]:
                 #move from current index to the next                
                 current_idx += 1
-            #otherwise, store the value of current index as the floor/valley value                
+            #store the value of current index as the floor/valley value                
             peak_price = prices[current_idx]
+            #calculate maximum profit by adding up the difference of peak value and valley value
             max_profit += peak_price - valley_price
         return max_profit
