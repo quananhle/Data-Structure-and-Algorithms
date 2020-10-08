8ff6e0c32f9a7cd9b8bf584ef8adb36257409362
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-10-07 23:42:53 -0500

    Update PeakValley.py

--- Python/Maximizing-Profit/Buy-Sell-Stock-II/PeakValley.py
+++ Python/Maximizing-Profit/Buy-Sell-Stock-II/PeakValley.py
@@ -5,17 +5,21 @@ class PeakValley(object):
         :rtype: int
         """
         current_idx  = 0
-        valley_price = 0
-        peak_price   = 0
+        next_idx     = current_idx + 1
         max_profit   = 0
         #loop until the last element of list prices
         while current_idx < len(prices)-1:
-            #loop if not 
-            while current_idx < len(prices)-1 and prices[current_idx] >= prices[current_idx+1]:
+            #loop if not at the last index and value of the current index is higher than the value of the next index
+            while current_idx < len(prices)-1 and prices[current_idx] >= prices[next_idx]:
+                #move from current index to the next
                 current_idx += 1
-            valley_prices = prices[current_idx]
-            while current_idx < len(prices)-1 and prices[current_idx] <= prices[current_idx+1]:
+            #otherwise, store the value of current index as the floor/valley value
+            valley_price = prices[current_idx]
+            #loop if not at the last index and value of the current index is lower than the value of the next index            
+            while current_idx < len(prices)-1 and prices[current_idx] <= prices[next_idx]:
+                #move from current index to the next                
                 current_idx += 1
-            peak_prices = prices[current_idx]
-            max_profit += peak_prices - valley_prices
+            #otherwise, store the value of current index as the floor/valley value                
+            peak_price = prices[current_idx]
+            max_profit += peak_price - valley_price
         return max_profit
