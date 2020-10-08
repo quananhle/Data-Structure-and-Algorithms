3e2b5d6ee26678eb423ed8ff23d0e1e5838efef2
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-10-07 23:26:14 -0500

    Update PeakValley.py

--- Python/Maximizing-Profit/Buy-Sell-Stock-II/PeakValley.py
+++ Python/Maximizing-Profit/Buy-Sell-Stock-II/PeakValley.py
@@ -8,11 +8,11 @@ class PeakValley(object):
         valley_price = 0
         peak_price   = 0
         max_profit   = 0
-        while i < (len(prices)-1):
-            while (i<len(prices)-1) and (prices[i] >= prices[i+1]):
+        while i < len(prices)-1:
+            while i < len(prices)-1 and prices[i] >= prices[i+1]:
                 i += 1
             valley_prices = prices[i]
-            while (i<len(prices)-1) and (prices[i] <= prices[i+1]):
+            while i < len(prices)-1 and prices[i] <= prices[i+1]:
                 i += 1
             peak_prices = prices[i]
             max_profit += peak_prices - valley_prices
