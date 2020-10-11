b48dcc6595f274f850138f8a89cb89cfe14d854f
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-10-10 20:20:29 -0500

    Create Solution.py

---
+++ Python/Array-Backed List/Find-Pivot-Index/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left_sum = 0
+        right_sum = sum(nums)
+        for i in range(len(nums)):
+            right_sum -= nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+        return -1
