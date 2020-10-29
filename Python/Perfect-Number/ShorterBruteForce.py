9dfc63e3f1047e8aee1f3a8baef0a64a7673b71b
Quananhle <35042430+Quananhle@users.noreply.github.com>
2020-10-29 00:51:54 -0500

    Create ShorterBruteForce.py

---
+++ Python/Perfect-Number/ShorterBruteForce.py
@@ -0,0 +1,17 @@
+class ShorterBruteForce {
+    public boolean checkPerfectNumber(int num) {
+        if(num <= 0) {
+          return false;  
+        }
+        int sum = 0;
+        for (int i=1; i <= num/2; ++i){
+            if(num % i == 0){
+                sum += i;
+            }
+            if(sum > num) {
+              return false;
+            }
+        }   
+        return sum == num;
+    }
+}
